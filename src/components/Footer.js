864fae09de0b79f58d83d8bf28d4edbeb1bf21d6
Puvan <puvanes77@live.com.sg>
2024-08-27 19:22:08 +0800

    minor fix footer padding adjusted and displayed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,21 +5,23 @@ import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
-    <footer className="footer" id="footer">
-        <Container>
-            <Row className="align-items-center">
-                <Col md={6}>
-                    <img src={logo} alt="Logo"></img>
-                </Col>
-                <Col md={6}>
-                    <div className="social-icon">
-                        <a href="/#"><img src={navIcon1} alt=""></img></a>
-                        <a href="/#"><img src={navIcon2} alt=""></img></a>
-                        <a href="/#"><img src={navIcon3} alt=""></img></a>
-                    </div>
-                    <p>Copyright 2024. All Rights Reserved</p>
-                </Col>
-            </Row>
-        </Container>
-    </footer>
+    return (
+        <footer className="footer" id="footer">
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                        <img src={logo} alt="Logo"></img>
+                    </Col>
+                    <Col md={6} className="text-center text-sm-end">
+                        <div className="social-icon">
+                            <a href="/#"><img src={navIcon1} alt=""></img></a>
+                            <a href="/#"><img src={navIcon2} alt=""></img></a>
+                            <a href="/#"><img src={navIcon3} alt=""></img></a>
+                        </div>
+                        <p>Copyright 2024. All Rights Reserved</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
 }
