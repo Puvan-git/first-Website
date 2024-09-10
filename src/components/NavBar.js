4ad118f8a5cd8b0639c58afc339ecee0be36215e
Puvan <puvanes77@live.com.sg>
2024-09-10 18:51:06 +0800

    adding of git, printer logos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import { Nav, Navbar, Container } from 'react-bootstrap';
 import { useState, useEffect } from "react";
 import logo from "../assets/img/logo.svg"; // change logo to initalP 
 import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/printer.png";
+import navIcon3 from "../assets/img/github-mark.png";
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState('home');
@@ -35,7 +37,7 @@ export const NavBar = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-auto nav-underline">
             <Nav.Link href="#home" className={activeLink === 'home' ?
               'active-navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
             <Nav.Link href="#skills" className={activeLink === 'skills' ?
@@ -48,6 +50,12 @@ export const NavBar = () => {
           <span className="navbar-text">
             <div className="social-icon">
               <a href="/#"><img src={navIcon1} alt=""></img></a>
+            </div>
+            <div className="social-icon">
+              <a href="/#"><img src={navIcon2} alt=""></img></a>
+            </div>
+            <div className="social-icon">
+              <a href="/#"><img src={navIcon3} alt=""></img></a>
             </div>
             <button className="vvd" onClick={() => console.log("connect")}><span>Contact Me</span></button>
           </span>
